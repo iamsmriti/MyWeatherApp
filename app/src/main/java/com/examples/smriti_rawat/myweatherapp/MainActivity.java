55786b808e73c2f9f2b7600ea3626a9e5d002ea7
SmritiRawat <smriti_rawat@SMRITIs-MacBook-Pro-2.local>
2017-05-19 02:00:47 +0530

    add polish

--- app/src/main/java/com/examples/smriti_rawat/myweatherapp/MainActivity.java
+++ app/src/main/java/com/examples/smriti_rawat/myweatherapp/MainActivity.java
@@ -11,19 +11,35 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
 import java.net.URL;
+import android.widget.ProgressBar;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mWeatherTextView;
+    private TextView mErrorMessageDisplay;
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
+        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+        loadWeatherData();
     }
     private void loadWeatherData(){
+        showWeatherDataView();
         String location= weatherpreferences.getPreferredWeatherLocation(this);
         new FetchWeatherData().execute(location);
+    }
+    private void showWeatherDataView() {
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        mWeatherTextView.setVisibility(View.VISIBLE);
+    }
+    private void showErrorMessage() {
+        mWeatherTextView.setVisibility(View.INVISIBLE);
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
     }
     public class FetchWeatherData extends AsyncTask<String ,Void ,String[]>{
         @Override
@@ -32,28 +48,26 @@ public class MainActivity extends AppCompatActivity {
             String location=params[0];
             URL weatherRequestUrl=NetworkUtils.buildUrl(location);
             try{
-                String jsonWeatherResponse = NetworkUtils
-                        .getResponseFromHttpUrl(weatherRequestUrl);
+                String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
                 System.out.println("jsonWeatherResponse " + jsonWeatherResponse);
-
-                String[] simpleJsonWeatherData = OpenWeatherJsonUtils
-                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
-
+                String[] simpleJsonWeatherData = OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
                 return simpleJsonWeatherData;
-
-            }catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
-
+            }catch (Exception e) { e.printStackTrace();return null;}
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
         }
         @Override
         protected void onPostExecute(String[] weatherData){
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
             if (weatherData != null) {
+                showWeatherDataView();
                 for (String weatherString : weatherData) {
                     mWeatherTextView.append((weatherString) + "\n\n\n");
                 }
-            }
+            } else {showErrorMessage();}
         }
     }
     @Override
